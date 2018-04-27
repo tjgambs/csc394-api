1d4ef6935a2311ea82bb40cb9dcfe84d68883e6d
cdittmer <crdittmer@outlook.com>
2018-04-27 16:59:47 -0500

    Change of names in Student.py as well as creation of Terminology documentation.

--- student.py
+++ student.py
@@ -1,38 +1,38 @@
 class Student(object):
-    previourMajor = ''
-    def __init__(self, Student_ID, Number_of_Classes_per_Quarter, Courses_Taken, Current_Quarter, Option_Type, Major, Specialty):
-       self.Student_ID = Student_ID
-       self.Number_of_Classes_per_Quarter = Number_of_Classes_per_Quarter
-       self.Courses_Taken = Courses_Taken
-       self.Current_Quarter = Current_Quarter
-       self.Option_Type = Option_Type
-       self.Major = Major
-       self.Specialty = Specialty
+    previourprogram = ''
+    def __init__(self, student_id, number_of_classes_per_quarter, courses_taken, current_quarter, program, elective_preference):
+       self.student_id = student_id
+       self.number_of_classes_per_quarter = number_of_classes_per_quarter
+       self.courses_taken = courses_taken
+       self.current_quarter = current_quarter
+       self.program = program
+       self.elective_preference = elective_preference
 
 
     def addCourse(self, Course_ID):
-        self.Courses_Taken.append(Course_ID)
+        self.courses_taken.append(Course_ID)
 
     def deleteCourse(self, Delete_Course_ID):
-        list = self.Courses_Taken
+        list = self.courses_taken
         for i in range(len(list)):
             if list[i] == Delete_Course_ID:
-               self.Courses_Taken.remove(Delete_Course_ID)
+               self.courses_taken.remove(Delete_Course_ID)
 
     def changeQuarter(self, New_Quarter):
-        self.Current_Quarter = New_Quarter
+        self.current_quarter = New_Quarter
 
+# TODO: Determine the type we want to use for the Quarter System
        # def changeQuarter(self):
-        #    self.Current_Quarter += 5
-        #  NOTE: Figure out how to format based onDatabase!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #    self.current_quarter += 5
+        #  NOTE: Figure out how to format based onDatabase
 
-    def changeSpecialty(self, New_Specialty):
-        self.Specialty = New_Specialty
+    def changeElectivePreference(self, New_elective_preference):
+        self.elective_preference = New_elective_preference
 
-    def changeMajor(self, New_Major):
-        Student.previousMajor = self.Major
-        self.Major = New_Major
+    def changeprogram(self, New_program):
+        Student.previous_program = self.program
+        self.program = New_program
 
-    def getPreviousMajor(self):
-        return Student.previousMajor
+    def getPreviousprogram(self):
+        return Student.previous_program
 
