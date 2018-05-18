f4f831050ea3f957acc29b1d2c50478cdde540e9
cdittmer <crdittmer@outlook.com>
2018-05-18 13:27:19 -0500

    Test

--- serialization.py
+++ serialization.py
@@ -3,7 +3,7 @@ import json
 def classListSerialize(classList):
     return json.dumps(classList)
 
-
+iojsdfoihdesfiondsfionhesfoin
 
 def deserializeForStudent(studentInfo):
     return studentInfo
