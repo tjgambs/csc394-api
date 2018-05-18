72419a327b858106be5389084dc96f2a68cdbc7c
cdittmer <crdittmer@outlook.com>
2018-05-18 14:04:24 -0500

    Test

--- serialization.py
+++ serialization.py
@@ -1,8 +1,15 @@
 import json
+from Student import Student
 
+# This will serialize the list  or (list of list) of classes that will be returned to the front end.
 def classListSerialize(classList):
     return json.dumps(classList)
 
-
+# This takes a JSON payload and will create a Student object which the API will be able to use.
 def deserializeForStudent(studentInfo):
-    return studentInfo
+    data = json.loads(studentInfo)
+    student_object = Student(data['student_id'], data['number_of_classes_per_quarter'], data['courses_taken']\
+        ,data['current_quarter'], data['curriculum'], data['elective_preference'])
+    return student_object
+
+
