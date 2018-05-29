8a7a6f870bdcd6a49074cede4162d2b9303cba9e
reigningsun <jeff.pancake@gmail.com>
2018-05-29 18:51:31 -0500

    Added an alternative automated search: --It looks at the current plans cost + stdCost instead of looking at the suggestedPlan.
    --Also uses the coursesTaken set as the key for the dict. This means that plans that took the same courses but in different order will recognize one another as alternative paths to the same point. This allows the algorithm to only add a plan if its better than all the alternative versions that got to the same point.
    
    --Seems to run much much faster. However, the results vary moreso than they did before.

--- app/logic/search.py
+++ app/logic/search.py
@@ -20,7 +20,7 @@ def heuristics(course, suggestedPlan, user):
 
         # Add bonus if course is preferred
         if course.getName in user.curriculum.courseTypeDesignations[userPref]:
-            bonus += 30
+            bonus += 20
 
         # If the course is a member of the users most frequently taken elective course type add a weight
         electivesCount = []
@@ -30,7 +30,7 @@ def heuristics(course, suggestedPlan, user):
         maxIdx = electivesCount.index(max(electivesCount)) + 5
 
         if course.getName in user.curriculum.courseTypeDesignations[maxIdx]:
-            bonus += 30
+            bonus += 40
 
     return score + bonus
 # =====================================================================================================================
@@ -59,7 +59,7 @@ def addUpdateCourse(suggestedPlan, suggestedCourseInfo, curriculum):
         suggestedPlan.addCourse(suggestedCourseInfo)
         courseTypeList = suggestedPlan.classifyCourse(suggestedCourseInfo, curriculum)
         suggestedPlan.incrCourseType(courseTypeList, suggestedPlan.typesTaken, curriculum.gradReqs)
-
+'''
 # =====================================================================================================================
 def automated(user):
     """Takes a user object and generates the shortest path to graduation. """
@@ -132,6 +132,7 @@ def automated(user):
 
             new_cost = costSoFar.get(suggestedPlan._id, costSoFar.get(current_plan._id, 0)) + stdCost
 
+
             print suggestedPlan.selectionOrder
             print suggestedPlan.typesTaken
 
@@ -139,7 +140,98 @@ def automated(user):
                 costSoFar[suggestedPlan._id] = new_cost
                 priority = new_cost + (stdCost - heuristics(suggestedCourseInfo, suggestedPlan, user))
                 frontier.put(suggestedPlan, priority)
-                cameFrom[suggestedPlan._id] = suggestedPlan
+                cameFrom[suggestedPlan._id] = current_plan # cameFrom[suggestedPlan._id] = suggestedPlan
+    print("Solution: ")
+    print current_plan.selectionOrder
+    print current_plan.typesTaken
+    return current_plan.selectionOrder
+# =====================================================================================================================
+'''
+
+
+# Trying this out. New take on it
+# =====================================================================================================================
+def automated(user):
+    """Takes a user object and generates the shortest path to graduation. """
+
+
+    # Start should be a Plan() and will include any classes the user has already taken to this point. It should have
+    # accurate counts of its course typesTaken before starting the search. Goal is the end state desired by the user.
+    # Usually this will be the gradReqs for the users Curriculum object, but this could also be used to create
+    # limited searches. number_of_classes_per_quarter is the max number of courses a user is willing to take in a
+    # given quarter.
+
+    # Cost should represent how many quarters are needed to graduate. They should however be multiplied by some factor
+    # so that they are larger than our heuristic values. Our heuristics should produce values for rarity that are 8 - #
+    # of times offered in 8 quarters. So the values will range between 0-8. Unlocks could be anything between 0 and
+    # the # of total classes opened up by taking it. My expectation is that a class will have less than 16 unlocks. If
+    # this is not the case then the cost formula needs to be revised. We never want to over-estimate the cost of a path
+    # to graduation. 100 is chosen because it is the maximum value of h(n) seen in our data.
+    # Should equal max(rarity) + max(unlocks) + bonus
+    # g(n) = (quarters x 100) or cost so far
+    # h(n) = 100 - (rarity + unlocks + bonus)
+    # f(n) = g(n) + h(n)
+    # Record actual cost of a path as g(n) / 100 = number of quarters to graduate.
+    # A course that is offered every quarter and unlocks nothing and does not match a preferred elective type will cost
+    # 100 which is exactly what adding a class costs. Selecting something more rare, and/or unlocks more classes will
+    # appear to cost less than a normal quarter. So the path will be an under-estimate of cost and therefore it will be
+    # admissible. As long as unlocks is less than 16 we will never have negative costs therefore h(n) will be considered
+    # consistent  .
+
+    start = Plan(
+        selectionOrder=list(),
+        coursesTaken=user.getCoursesTaken,
+        termNum=user.getTerm,
+        currTermIdx=0,
+        maxCourses=user.max_courses,
+        typesTaken=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    curriculum = user.curriculum
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    cameFrom = {}
+    costSoFar = {}
+    stdCost = 160 # 120               # TODO: The arbitrary constant cost of selecting a class described above
+
+    terms = ['0975', '0980', '0985', '0990', '0995', '1000', '1005']
+    queryResults = dict((term, TermCourses.getAvailableCourses(term)) for term in terms)
+
+    i = 0
+    while not frontier.empty():
+        print("Plans Popped: " + str(i))
+        print("Frontier Size: " + str(frontier.qsize()))
+        current_plan = frontier.get()
+        i += 1
+
+        if isGoal(current_plan, curriculum):
+            break
+
+        subsetResults = queryResults[TermCourses.convert_stream(current_plan.termNum)]
+
+        filteredResults = filter(subsetResults, current_plan, current_plan.daysFilled, curriculum)
+
+
+
+        for suggestedCourseInfo in filteredResults[:4]:                                         # number to keep
+            suggestedPlan = Plan(
+                selectionOrder=copy.deepcopy(current_plan.selectionOrder),
+                coursesTaken=copy.deepcopy(current_plan.coursesTaken),
+                termNum=copy.deepcopy(current_plan.termNum),
+                currTermIdx=copy.deepcopy(current_plan.currTermIdx),
+                maxCourses=user.max_courses,
+                typesTaken=copy.deepcopy(current_plan.typesTaken))
+            addUpdateCourse(suggestedPlan, suggestedCourseInfo, curriculum)
+
+            new_cost = costSoFar.get(str(current_plan.coursesTaken), costSoFar.get(str(current_plan.coursesTaken), 0)) + stdCost
+
+
+            print suggestedPlan.selectionOrder
+            print suggestedPlan.typesTaken
+
+            if str(suggestedPlan.coursesTaken) not in costSoFar or new_cost < costSoFar[str(suggestedPlan.coursesTaken)]:
+                costSoFar[str(suggestedPlan.coursesTaken)] = new_cost
+                priority = new_cost - heuristics(suggestedCourseInfo, suggestedPlan, user)
+                frontier.put(suggestedPlan, priority)
+                cameFrom[suggestedPlan._id] = current_plan # cameFrom[suggestedPlan._id] = suggestedPlan
     print("Solution: ")
     print current_plan.selectionOrder
     print current_plan.typesTaken
